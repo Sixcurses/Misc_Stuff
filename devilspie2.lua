e55ac4565473dae06976d047e68806d9b0e2fef5
Sixcurses <rickbark@gmail.com>
2021-09-24 10:46:28 +0200

    Update devilspie2.lua

--- devilspie2.lua
+++ devilspie2.lua
@@ -1,9 +1,12 @@
+function sleep(n)
+  os.execute("sleep " .. tonumber(n))
+end
+
 if (get_application_name() == "FINAL FANTASY XIV") then
   sleep(2)
   set_window_position(0,0);
   maximize();
 end
 
-function sleep(n)
-  os.execute("sleep " .. tonumber(n))
-end
+-- REGEX search app name
+-- if (string.find(get_application_name(),"what_to_search_for")~=nil) then
